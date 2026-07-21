fad308234a632690cfc3c130f4062540ee4aa80e
Yilmar <yilmar.riascos@correounivalle.edu.co>
2026-07-20 21:56:04 -0500

    primer commit del curso de javascript, se agrego 01-fundamentos

---
+++ 01-fundamentos/app.js
@@ -0,0 +1,6 @@
+// console.log() es una función que imprime valores en la consola.
+// Se usa para ver qué está pasando en el código (debug) mientras programas.
+// No devuelve nada útil (retorna undefined), solo muestra info en pantalla.
+
+// console.log("Hello, World!");   
+
