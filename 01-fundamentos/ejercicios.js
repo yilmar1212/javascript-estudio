c4fd2d99a4c17fd26499b1f25140ad642ea2fd74
Yilmar <yilmar.riascos@correounivalle.edu.co>
2026-07-24 22:01:42 -0500

    ejercicios añadidos: para comprender el uso de javascript

---
+++ 01-fundamentos/ejercicios.js
@@ -0,0 +1,40 @@
+/*function ejemplo (){
+    let r = console.log("holas");
+    return "r";
+}
+
+console.log(ejemplo());*/
+
+
+
+/*let mensaje = "hola mundo";
+
+console.log(mensaje);*/
+
+
+
+/*let nombre = "Juan";
+
+console.log("Mi nombre es " + nombre);*/
+
+
+/*let nombre = "Juan";
+let edad = 20;
+
+console.log(nombre + " tiene " + edad + " años ");*/
+
+
+
+/* function saludar(nombre){
+    console.log("hola " + nombre );
+}
+
+saludar ("carlos"); */
+
+
+
+function saludar(nombre){
+    return "hola " + nombre;
+}
+
+console.log(saludar("Ana"));
