c0a82d091d0ca846aa9f6e2368491ec44f76fa26
Yilmar <yilmar.riascos@correounivalle.edu.co>
2026-09-03 23:09:53 -0500

    problema clases: se demostro la manera antigua de crear clases

--- 01-fundamentos/teoria/classes/problema.js
+++ 01-fundamentos/teoria/classes/problema.js
@@ -1,3 +1,48 @@
 
 
 
+const fher = {
+    nombre: "Fernando",
+    edad: 30,
+    imprimir() {                // cuando se quiera hacer una referencia no lo llames con. ejemplo: fher.nombre
+        console.log(`Nombre: ${ this.nombre } - Edad: ${ this.edad }`);
+        
+    }
+}
+
+
+const pedro = {
+    nombre: "Pedro",
+    edad: 15,
+    imprimir() {                // cuando se quiera hacer una referencia no lo llames con. ejemplo: fher.nombre
+    console.log(`Nombre: ${ this.nombre } - Edad: ${ this.edad }`);
+        }
+    }
+
+
+
+
+
+// fher.imprimir ();
+
+         // con P mayuscula para indicar que es oper camell case, o como se llame 
+function Persona (nombre, edad) {
+    console.log("se ejecuto esta linea");
+    this.nombre = nombre;
+    this.edad = edad;
+
+    this.imprimir = function() {
+         console.log(`Nombre: ${ this.nombre } - Edad: ${ this.edad }`);
+    }
+}
+
+              // palabra new es para crear instancias
+const maria = new Persona (`Maria`, 18)
+const melissa = new Persona (`Melissa`, 35)
+
+console.log(maria);
+maria.imprimir();
+melissa.imprimir();
+
+
+// todo este codigo anterior es lo que hacian para crear "clases", por asi decirlo es un metodo antiguo
