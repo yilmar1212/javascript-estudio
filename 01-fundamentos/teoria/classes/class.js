9d750f7dbee89bf73572c17ad8d5acba336f529c
Yilmar <yilmar.riascos@correounivalle.edu.co>
2026-09-05 18:35:57 -0500

    datos sobre la forma de usar clases

---
+++ 01-fundamentos/teoria/classes/class.js
@@ -0,0 +1,11 @@
+
+
+
+class Persona {
+    constructor() {  // el constructor es un metodo que se va a ejecutar en el momento que se crea una nueva instancia de la clase
+        console.log ("hola! "); 
+    }
+}
+
+
+const spiderman = new Persona();
